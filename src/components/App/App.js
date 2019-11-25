bd84d2f709540aa127c0730d433f2232f17100d9
Andrew KIl <kil.andrew@gmail.com>
2019-11-25 16:38:07 -0500

    add connect and mapStateToProps to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { connect } from "react-redux";
 import Game from "../Game";
 
 import "./App.css";
@@ -12,4 +12,9 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  board: state.board,
+  player: state.player
+});
+
+export default connect(mapStateToProps)(App);
