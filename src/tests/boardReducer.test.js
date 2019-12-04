f6e912f34bb7aadb22230a2ece26392d35e65592
Andrew KIl <kil.andrew@gmail.com>
2019-12-03 19:25:47 -0500

    add boardReducer tests

--- src/tests/boardReducer.test.js
+++ src/tests/boardReducer.test.js
@@ -1,16 +1,23 @@
 import boardReducer from "../reducers/boardReducer";
-import { DROP_CHECKER } from "../actions/constants";
+import { PLAY_TURN } from "../actions/constants";
 
 describe("boardReducer tests", () => {
   it("should play first move appropriately", () => {
     const sampleAction = {
-      type: DROP_CHECKER,
+      type: PLAY_TURN,
       payload: {
         player: 1,
         column: 0
       }
     };
-    const sampleInputBoard = Array(6).fill(Array(7).fill(0));
+    const sampleInputBoard = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0]
+    ];
     const expectedOutputBoard = [
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
@@ -25,7 +32,7 @@ describe("boardReducer tests", () => {
 
   it("should play second move appropriately", () => {
     const sampleAction = {
-      type: DROP_CHECKER,
+      type: PLAY_TURN,
       payload: {
         player: 2,
         column: 0
@@ -53,7 +60,7 @@ describe("boardReducer tests", () => {
 
   it("should return prevState when column is full", () => {
     const sampleAction = {
-      type: DROP_CHECKER,
+      type: PLAY_TURN,
       payload: {
         player: 1,
         column: 0
