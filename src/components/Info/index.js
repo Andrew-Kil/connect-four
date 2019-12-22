5a2cd758456a736dec5b13ee3b4e5add8081b5bf
Andrew KIl <kil.andrew@gmail.com>
2019-12-22 13:53:06 -0500

    update info text

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -21,12 +21,13 @@ class Info extends Component {
         </button>
         <div
           className={showInfoText ? `${s.showInfoText}` : `${s.hideInfoText}`}>
-          Connect 4 is a two-player game. After determining who goes first, the
-          players take turns dropping red or yellow tokens into a seven-column,
-          six-row grid. The pieces fall from top to bottom, occupying the next
-          available space within the column. The objective of the game is to
-          connect four tokens of the same color vertically, horizontally, or
-          diagonally before your opponent.
+          Connect 4 is a two-player game where the objective is to connect four
+          tokens of the same color vertically, horizontally, or diagonally
+          before your opponent. Player 1 uses red tokens while Player 2 uses
+          yellow tokens. After determining who goes first, the players take
+          turns dropping their tokens into a seven-column, six-row grid. The
+          pieces fall from top to bottom, occupying the next available space
+          within the column.
         </div>
       </>
     );
